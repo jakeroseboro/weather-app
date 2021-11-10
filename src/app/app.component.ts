734a26d71c2e50849421c35e8d84a2cfb819af52
jakeroseboro <57158408+jakeroseboro@users.noreply.github.com>
2021-11-10 14:53:02 -0600

    begin styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,21 +12,29 @@ export class AppComponent implements OnInit{
   title = 'weather-app';
   searchResult: string;
   weatherData: any;
+  isValidResponse: boolean = true;
 
   constructor(
     public weatherSvc: WeatherService,
     public route: ActivatedRoute,
   ){};
 
-  ngOnInit(){
-    const response = this.weatherSvc.getWeatherForCity('Pensacola').subscribe(
-      data => {this.weatherData = data}
+  async ngOnInit(){
+    const response = await this.weatherSvc.getWeatherForCity('Pensacola').subscribe(
+      data => {this.weatherData = data, console.log(data)}
     )
   }
 
   async onSearch(){
-    const response = this.weatherSvc.getWeatherForCity(this.searchResult).subscribe(
+    const response = await this.weatherSvc.getWeatherForCity(this.searchResult).subscribe(
       data => {this.weatherData = data}
     )
+  }
+
+  getColor(){
+    if(this.weatherData.weather[0].description === "clear sky"){
+      return 'sunny'
+    }
+    else return 'sunny'
   }
 }
